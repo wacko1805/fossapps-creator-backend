41a0d3c5991ad68892faad444f56b160ed33275b
jack <jackccsam@gmail.com>
2021-12-05 14:40:31 +1100

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,33 @@
+
+
 <html>
     <head>
         <script type="text/javascript" src="assets/js/jszip.js"></script>
         <script type="text/javascript" src="assets/js/jszip-utils.js"></script>
         <script src="assets/js/FileSaver.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
+        <link rel="stylesheet" href="https://un.pixel-fy.com/assets/css/style.css">
        </head>
-    <body>
-        <button id="data_uri" onclick="test()" class="btn btn-primary">click to download</button>
+    <body onload="test()">
+    <div id="overlay-back"></div>
+<div id="overlay">
+    <div id="dvLoading">
+        <h2>Please wait</h2>
+        <p>This may take a very long time....</p>
+        <div class="bottom"><h2>Downloading selected apps and putting into a fashable zip...</h2>
+        <p><p>DO NOT refresh or leave this page until download has started</p><br>
+        </div>
+
+        <div class="main">
+          <div class="one"></div>
+          <div class="two"></div>
+          <div class="three"></div>
+        </div>
+    </div>
+</div>
+<script>
+              $('#dvLoading, #overlay, #overlay-back').fadeIn(500);
+            </script>
         </body>
     <script>
         
@@ -20,9 +41,17 @@ var urls = [
 ];
 
 var zip = new JSZip();
+    var moduleprop = $.get("base/module.prop");
+    var customize = $.get("base/customize.sh");
+    var uninstall = $.get("base/uninstall.sh");
+    var updatebin = $.get("base/META-INF/com/google/android/update-binary");
+    var updaterscript = $.get("base/META-INF/com/google/android/updater-script");
+    var funcions = $.get("base/common/functions.sh");
+    var install = $.get("base/common/install.sh");
+
     var count = 0;
     var count2 = 0;
-    var zipFilename = "zipFilename.zip";
+    var zipFilename = "Fossapps-Custom.zip";
     var nameFromURL = [];
 
     var the_arr = "";
@@ -41,7 +70,16 @@ var zip = new JSZip();
             if(err) {
                 throw err; // or handle the error
             }
-            zip.file("system/app/" + foldername + "/" + filename, data, {binary:true});
+      
+            zip.file("module.prop", moduleprop);
+            zip.file("customize.sh", customize);
+            zip.file("uninstall.sh", uninstall);
+            zip.file("META-INF/com/google/android/update-binary", updatebin);
+            zip.file("META-INF/com/google/android/updater-script", updaterscript);
+            zip.file("common/functions.sh", funcions);
+            zip.file("common/install.sh", install);
+
+            zip.file("system/product/app/" + foldername + "/" + filename, data, {binary:true});
             count++;
             if (count === urls.length) {
                 zip.generateAsync({type:'blob'}).then(function(content) {
